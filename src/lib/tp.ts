286ba51675a3b66c633f57555206538a8743527f
jungltemple <146572102+jungltemple@users.noreply.github.com>
2025-06-15 16:28:21 +0100

    feat: return to development, lose track of last changes so call this commit feat

--- src/lib/tp.ts
+++ src/lib/tp.ts
@@ -1,12 +1,12 @@
 export function tHead(intensity: number = 1) {
   switch (intensity) {
     case 1:
-      return "text-4xl font-bold";
+      return "text-4xl font-bold font-mono";
     case 2:
-      return "text-3xl font-light";
+      return "text-3xl font-light font-mono";
     case 3:
-      return "text-2xl font-bold";
+      return "text-2xl font-bold font-mono";
     default:
-      return "text-2xl font-bold";
+      return "text-2xl font-bold font-mono";
   }
 }
